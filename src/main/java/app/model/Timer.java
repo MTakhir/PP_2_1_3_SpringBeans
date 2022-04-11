40e049b85526cac13893d82b3b9346f30bbc35f5
Takhir Musakhanov <m.taha@mail.ru>
2022-04-11 14:57:13 +0400

    Исправил бины

--- src/main/java/app/model/Timer.java
+++ src/main/java/app/model/Timer.java
@@ -3,7 +3,6 @@ package app.model;
 import org.springframework.stereotype.Component;
 
 import java.io.Serializable;
-import java.util.Objects;
 
 @Component
 public class Timer implements Serializable {
